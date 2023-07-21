ce78ecea6cd450b062c2904ef3e8f1a1e7514273
PRAGYA CHURENDRA <109157180+Pragya-1801@users.noreply.github.com>
2023-07-21 22:45:29 +0530

    Add files via upload

---
+++ SQLQueryswiggy.sql
@@ -0,0 +1,54 @@
+select * from [dbo].[swiggy]
+
+#Q1 HOW MANY RESTAURANTS HAVE A RATING GREATER THAN 4.5?
+
+select count(distinct restaurant_name) 
+as high_rated_restaurants
+from swiggy
+where rating>4.5;
+
+#Q2 WHICH IS THE TOP 1 CITY WITH THE HIGHEST NUMBER OF RESTAURANTS?
+
+select city,count(distinct restaurant_name) 
+as restaurant_count from swiggy
+group by city
+order by restaurant_count desc
+limit 1;
+
+#Q3 HOW MANY RESTAURANTS HAVE THE WORD "PIZZA" IN THEIR NAME?
+select count(distinct restaurant_name) as pizza_restaurants
+from swiggy 
+where restaurant_name like '%Pizza%';
+
+#Q4 WHAT IS THE MOST COMMON CUISINE AMONG THE RESTAURANTS IN THE DATASET?
+
+select cuisine,count(*) as cuisine_count
+from swiggy
+group by cuisine
+order by cuisine_count desc
+limit 1;
+
+#Q5 WHAT IS THE AVERAGE RATING OF RESTAURANTS IN EACH CITY?
+select city, avg(rating) as average_rating
+from swiggy group by city;
+
+#Q6 WHAT IS THE HIGHEST PRICE OF ITEM UNDER THE 'RECOMMENDED' MENU
+CATEGORY FOR EACH RESTAURANT?
+
+select distinct restaurant_name,
+menu_category,max(price) as highestprice
+from swiggy where menu_category='Recommended'
+group by restaurant_name,menu_category;
+
+#Q7 FIND THE TOP 5 MOST EXPENSIVE RESTAURANTS THAT OFFER CUISINE OTHER THAN
+INDIAN CUISINE. 
+select distinct restaurant_name,cost_per_person
+from swiggy where cuisine<>'Indian'
+order by cost_per_person desc
+limit 5;
+
+#Q8 WHICH IS THE RESTAURANT PROVIDING THE LOWEST AVERAGE PRICE FOR ALL ITEMS?
+select distinct restaurant_name,
+avg(price) as average_price
+from swiggy group by restaurant_name
+order by average_price limit 1;
